9510968c0427049a4ef7f11da312231a14caf554
alq <1077982932@qq.com>
2026-03-29 19:22:52 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Auther: alq
+ * @Date: 2026/3/29 - 03 - 29 - 19:22
+ * @Description: org.example
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
